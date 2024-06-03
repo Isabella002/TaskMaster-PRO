2cd400e81690cbfed0750285cb0b005a8a4e7531
isabella002 <isabella.mester@gmail.com>
2024-06-02 17:51:20 -0700

    index

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
 export const formatDate = (date) => {
-  // Get the month, day, and year
+
   const month = date.toLocaleString("en-US", { month: "short" });
   const day = date.getDate();
   const year = date.getFullYear();
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dbConnection();
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:3000", "http://localhost:3001", "https://taskmanager-f6611.firebaseapp.com", "https://taskmanager-f6611.web.app"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
 
 app.use(cors({
   origin: (origin, callback) => {
@@ -38,3 +38,22 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Server listening on ${port}`));
+
+fetch('https://taskmanager-f6611.web.app/api/user/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  credentials: 'include', // Include credentials (cookies, authorization headers, etc.)
+  body: JSON.stringify({
+    username: 'your-username',
+    password: 'your-password',
+  }),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
