d97a5b13ea4b1369fd4d23ed201f64de2d7a67b6
isabella002 <isabella.mester@gmail.com>
2024-06-05 13:27:17 -0700

    changes

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,21 +1,25 @@
-const routeNotFound = (req, res, next) => {
+// server/middleware/errorMiddleware.js
+export const routeNotFound = (req, res, next) => {
   const error = new Error(`Route Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
-const errorHandler = (err, req, res, next) => {
-  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  let message = err.message;
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    statusCode = 404;
-    message = "Resource not found";
-  }
-
-  res.status(statusCode).json({
-    message: message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+export const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
 };
 
-export { routeNotFound, errorHandler };
+// Middleware to handle JSON parsing errors
+export const handleJSONParsingError = (err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.error('JSON Parsing Error:', err);
+    return res.status(400).send({ message: 'Invalid JSON' });
+  }
+  next(err);
+};
+
